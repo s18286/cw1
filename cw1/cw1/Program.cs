bb92ba7a679b78858fb1107cdf9f7bb2665ca8a2
Bartosz Gertych <s18286@pjwstk.edu.pl>
2020-03-01 18:07:24 +0100

    zad4

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (var a in args)
+            {
+                Console.WriteLine(a);
+            }
+
+            var emails = await GetEmails(args[0]);
+
+            foreach (var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+
+        }
+
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var listOfEmails = new List<string>();
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+            MatchCollection emailMatches = emailRegex.Matches(response.Content.ReadAsStringAsync().Result);
+
+
+            foreach(var emailMatch in emailMatches)
+            {
+                listOfEmails.Add(emailMatch.ToString());
+            }
+            return listOfEmails;
         }
     }
 }
